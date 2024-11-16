4aec83c0c28d5ce6d5cff5b3c380937e8eccc4ea
zhbforum <lyosha.minakoff@gmail.com>
2024-11-16 20:23:00 +0100

    Changes

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from "jwt-decode";
 
-function ProtectedRoute({ children, allowedRole }) 
-{
+function ProtectedRoute({ children, allowedRole }) {
     const token = localStorage.getItem('token');
 
-    if (!token) 
-    {
-        return <Navigate to="/" />;
+    if (!token) {
+        return <Navigate to="/login" />;
     }
 
-    try 
-    {
-        const decoded = jwtDecode(token);
-
-        if (decoded.role !== allowedRole) 
-        {
+    try {
+        const decoded = jwtDecode(token); // Используем именованный импорт
+        if (decoded.role !== allowedRole) {
             return <Navigate to="/" />;
         }
     } catch (error) {
         console.error('Ошибка токена:', error);
-        return <Navigate to="/" />;
+        return <Navigate to="/login" />;
     }
 
     return children;
