5ed7464abe93591a41a0c07fa570f142dba2de6d
killursxlf <excgerty@gmail.com>
2024-11-16 20:51:16 +0300

    Few fixes

--- frontend/src/pages/LoansPage.jsx
+++ frontend/src/pages/LoansPage.jsx
@@ -2,18 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-function LoansPage() 
-{
+function LoansPage() {
   const [loans, setLoans] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3000/loans')
-      .then(response => 
-      {
+      .then(response => {
         setLoans(response.data);
       })
-      .catch(error => 
-      {
+      .catch(error => {
         console.error('Error while retrieving position data:', error);
       });
   }, []);
@@ -22,30 +19,35 @@ function LoansPage()
     <div>
       <h1>List of loans</h1>
       {loans.length === 0 ? (
-        <p>No loans found.</p> 
+        <p>No loans found.</p>
       ) : (
         <ul>
-          {loans
-            .map(loan => {
-              const formattedDate = new Date(loan.loan_date).toLocaleDateString('en-GB'); 
-  
-              return (
-                <li key={loan.id} style={{ marginBottom: '10px' }}>
-                  <div>Reader: {loan.reader_name} - Book: {loan.book_title}</div>
-                  <div>Date of Issue: {formattedDate} - Fine: ${loan.fine}</div>
-                  <Link to={`/loans/editing/${loan.id}`}>
-                    <button>Edit</button>
-                  </Link>
-                </li>
-              );
-            })}
+          {loans.map(loan => {
+            const formattedDate = new Date(loan.loan_date).toLocaleDateString('en-GB'); 
+
+            return (
+              <li key={loan.id} style={{ marginBottom: '10px' }}>
+                <div>
+                  Reader: <span className="bold-text">{loan.reader_name}</span> - 
+                  Book: <span className="bold-text">{loan.book_title}</span>
+                </div>
+                <div>
+                  Date of Issue: <span className="bold-text">{formattedDate}</span> - 
+                  Fine: <span className="bold-text">${loan.fine}</span>
+                </div>
+                <Link to={`/loans/editing/${loan.id}`}>
+                  <button>Edit</button>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       )}
       <Link to="/loans/editing">
         <button>Add a new loan</button>
       </Link>
     </div>
-  );  
-}  
+  );
+}
 
 export default LoansPage;
