0908e71bfd6cee4152e332a517f025c92bb85ad3
killursxlf <excgerty@gmail.com>
2024-11-16 20:01:16 +0300

    Improve search

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,6 +10,7 @@ function Header() {
       </Link>
       <nav>
         <ul>
+          <li><Link to="/advanced-search">Search</Link></li>
           <li><Link to="/books">Books</Link></li>
           <li><Link to="/authors">Authors</Link></li>
           <li><Link to="/readers">Readers</Link></li>
