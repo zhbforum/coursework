d204c153cee93662900670e50222c83efd931820
zhbforum <lyosha.minakoff@gmail.com>
2024-11-17 03:08:49 +0100

    Protect route was fixed

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode";
 
-function ProtectedRoute({ children, allowedRole }) {
+function ProtectedRoute({ children, allowedRole }) 
+{
     const token = localStorage.getItem('token');
 
-    if (!token) {
+    if (!token) 
+    {
         return <Navigate to="/login" />;
     }
 
-    try {
+    try 
+    {
         const decoded = jwtDecode(token); 
-        if (decoded.role !== allowedRole) {
+        if (decoded.role !== allowedRole) 
+        {
             return <Navigate to="/" />;
         }
-    } catch (error) {
-        console.error('Ошибка токена:', error);
+    } 
+    catch (error) 
+    {
+        console.error('Token error:', error);
         return <Navigate to="/login" />;
     }
 
